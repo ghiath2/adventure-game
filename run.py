0ebb789bd944e28951abacd74b4a6984ffe50a9e
ghiath2 <ghiathht3@gmail.com>
2022-04-23 16:41:14 +0000

    Add basic functions to the game

--- run.py
+++ run.py
@@ -5,3 +5,29 @@ import sys
 import os
 import random
 import time
+
+
+class Game:
+
+    def print_slow(self, str, delay=0.1):
+        for letter in str:
+            sys.stdout.write(letter)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print("\n")
+
+
+    def reset_console(self):
+        print("\n")
+        os.system('cls||clear')
+
+
+    def fprint(self, str, delay=0):
+        print("\n" + str)
+        time.sleep(delay)
+
+
+    def sprint(self, str, delay=0):
+        print(str)
+        time.sleep(delay)
+
